c144b69ab349b86e48883306b312bb9c7aaf568c
Gustavo <gustavopolonio1@gmail.com>
2024-12-26 14:22:58 -0300

    test(update org): add test for preventing update unexisting org

--- src/use-cases/update-org.spec.ts
+++ src/use-cases/update-org.spec.ts
@@ -1,6 +1,7 @@
 import { beforeEach, describe, expect, it } from 'vitest'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 import { UpdateOrgUseCase } from './update-org'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
 
 let orgsRepository: InMemoryOrgsRepository
 let sut: UpdateOrgUseCase
@@ -75,5 +76,20 @@ describe('Update org use case', () => {
       }),
     )
     expect(orgsRepository.orgs).toHaveLength(1)
+  })
+
+  it('should not be able to update an unexisting org', async () => {
+    await expect(
+      sut.execute({
+        id: 'unexisting-id',
+        name: 'Org 2',
+        email: 'org2@test.test',
+        password: '123456',
+        zipCode: '13566-583',
+        address: 'Rua Tomaz Antonio Gonzaga, 382',
+        city: 'São Carlos',
+        whatsapp: '16 99399-0990',
+      }),
+    ).rejects.toBeInstanceOf(ResourceNotFoundError)
   })
 })
