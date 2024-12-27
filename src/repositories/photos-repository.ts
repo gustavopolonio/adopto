d5a5112864e52134eb22167435fa41fc85399ba1
Gustavo <gustavopolonio1@gmail.com>
2024-12-27 15:57:32 -0300

    feat: create photos repository pattern with getManyByPetId, create and createMany methods

---
+++ src/repositories/photos-repository.ts
@@ -0,0 +1,7 @@
+import { Photo, Prisma } from '@prisma/client'
+
+export interface PhotosRepository {
+  getManyByPetId(petId: string): Promise<Photo[] | []>
+  create(data: Prisma.PhotoUncheckedCreateInput): Promise<Photo>
+  createMany(data: Prisma.PhotoUncheckedCreateInput[]): Promise<Photo[]>
+}
