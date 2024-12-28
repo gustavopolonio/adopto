c92d1e45739d1eb23ca7945eca17fbaefb18c064
Gustavo <gustavopolonio1@gmail.com>
2024-12-27 22:46:40 -0300

    feat: create delete org use case

---
+++ src/use-cases/delete-org.ts
@@ -0,0 +1,34 @@
+import { Org } from '@prisma/client'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+import { OrgsRepository } from '@/repositories/orgs-repository'
+import { PetsRepository } from '@/repositories/pets-repository'
+
+interface DeleteOrgUseCaseRequest {
+  id: string
+}
+
+interface DeleteOrgUseCaseResponse {
+  org: Org
+}
+
+export class DeleteOrgUseCase {
+  constructor(
+    private orgsRepository: OrgsRepository,
+    private petsRepository: PetsRepository,
+  ) {}
+
+  async execute({
+    id,
+  }: DeleteOrgUseCaseRequest): Promise<DeleteOrgUseCaseResponse> {
+    const org = await this.orgsRepository.findById(id)
+
+    if (!org) {
+      throw new ResourceNotFoundError()
+    }
+
+    await this.orgsRepository.delete(id)
+    await this.petsRepository.deleteManyByOrgId(id)
+
+    return { org }
+  }
+}
