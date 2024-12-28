0041fe6046bb39293ac52640d8e4dc9788fb4bf5
Gustavo <gustavopolonio1@gmail.com>
2024-12-28 11:38:32 -0300

    test(get pets from city): add test for get pets from existing city filtered by age in months

--- src/use-cases/get-pets-from-city.spec.ts
+++ src/use-cases/get-pets-from-city.spec.ts
@@ -154,4 +154,69 @@ describe('Get pets from city use case', () => {
     expect(pets).toHaveLength(0)
     expect(pets).toEqual([])
   })
+
+  it('should be able to get pets from existing city filtered by age in months', async () => {
+    const city = 'São Carlos'
+    const ageInMonths = 12
+
+    const org = await orgsRepository.create({
+      name: 'Org 1',
+      email: 'org1@test.test',
+      password_hash: await hash('123456', 6),
+      zip_code: '13566-583',
+      address: 'Rua Tomaz Antonio Gonzaga, 382',
+      city,
+      whatsapp: '16 99399-0990',
+    })
+
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 1',
+      description: 'Description 1',
+      age_in_months: ageInMonths,
+      size: 'MEDIUM',
+      energy_level: 'HIGH',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 2',
+      description: 'Description 2',
+      age_in_months: ageInMonths,
+      size: 'MEDIUM',
+      energy_level: 'HIGH',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 3',
+      description: 'Description 3',
+      age_in_months: 14,
+      size: 'MEDIUM',
+      energy_level: 'HIGH',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    const { pets } = await sut.execute({
+      city,
+      page: 1,
+      ageInMonths,
+    })
+
+    expect(pets).toHaveLength(2)
+    expect(pets).toEqual([
+      expect.objectContaining({
+        org_id: org.id,
+        name: 'Pet 1',
+        description: 'Description 1',
+      }),
+      expect.objectContaining({
+        org_id: org.id,
+        name: 'Pet 2',
+        description: 'Description 2',
+      }),
+    ])
+  })
 })
