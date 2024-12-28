b854a58a9421c3d1458bc3c2f9fd47cce97879d0
Gustavo <gustavopolonio1@gmail.com>
2024-12-28 12:33:30 -0300

    test(get pets from city): add test for get pets from existing city ordered by most recent added

--- src/use-cases/get-pets-from-city.spec.ts
+++ src/use-cases/get-pets-from-city.spec.ts
@@ -1,4 +1,4 @@
-import { beforeEach, describe, expect, it } from 'vitest'
+import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
 import { hash } from 'bcryptjs'
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
 import { GetPetsFromCityUseCase } from './get-pets-from-city'
@@ -10,9 +10,15 @@ let sut: GetPetsFromCityUseCase
 
 describe('Get pets from city use case', () => {
   beforeEach(() => {
+    vi.useFakeTimers()
+
     orgsRepository = new InMemoryOrgsRepository()
     petsRepository = new InMemoryPetsRepository(orgsRepository)
     sut = new GetPetsFromCityUseCase(petsRepository)
+  })
+
+  afterEach(() => {
+    vi.useRealTimers()
   })
 
   it('should be able to get pets from existing city', async () => {
@@ -281,6 +287,84 @@ describe('Get pets from city use case', () => {
         org_id: org.id,
         name: 'Pet 3',
         description: 'Description 3',
+      }),
+    ])
+  })
+
+  it('should be able to get pets from existing city ordered by most recent added', async () => {
+    const city = 'São Carlos'
+
+    const org = await orgsRepository.create({
+      name: 'Org 1',
+      email: 'org1@test.test',
+      password_hash: await hash('123456', 6),
+      zip_code: '13566-583',
+      address: 'Rua Tomaz Antonio Gonzaga, 382',
+      city,
+      whatsapp: '16 99399-0990',
+    })
+
+    vi.setSystemTime(new Date(2000, 0, 1, 10, 0, 0, 0))
+
+    // First created pet
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 1',
+      description: 'Description 1',
+      age_in_months: 12,
+      size: 'MEDIUM',
+      energy_level: 'LOW',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    vi.setSystemTime(new Date(2000, 0, 1, 10, 0, 0, 1))
+
+    // Second created pet
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 2',
+      description: 'Description 2',
+      age_in_months: 12,
+      size: 'LARGE',
+      energy_level: 'HIGH',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    vi.setSystemTime(new Date(2000, 0, 2, 10, 0, 0, 0))
+
+    // Third created pet
+    await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 3',
+      description: 'Description 3',
+      age_in_months: 14,
+      size: 'LARGE',
+      energy_level: 'LOW',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    const { pets } = await sut.execute({
+      city,
+      page: 1,
+      sortBy: 'mostRecent',
+    })
+
+    expect(pets).toHaveLength(3)
+    expect(pets).toEqual([
+      expect.objectContaining({
+        org_id: org.id,
+        name: 'Pet 3',
+        description: 'Description 3',
+      }),
+      expect.objectContaining({
+        org_id: org.id,
+        name: 'Pet 2',
+        description: 'Description 2',
+      }),
+      expect.objectContaining({
+        org_id: org.id,
+        name: 'Pet 1',
+        description: 'Description 1',
       }),
     ])
   })
