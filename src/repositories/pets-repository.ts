66105c3903d164d613c6c0c6705ecbd778b2a008
Gustavo <gustavopolonio1@gmail.com>
2024-12-28 10:17:03 -0300

    feat: create findManyByCity method in pets repository pattern

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -2,6 +2,7 @@ import { Pet, Prisma } from '@prisma/client'
 
 export interface PetsRepository {
   findById(id: string): Promise<Pet | null>
+  findManyByCity(city: string, page: number): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   save(pet: Pet): Promise<Pet>
   delete(id: string): Promise<void>
