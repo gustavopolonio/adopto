f5f7e541ac3d6af754b9b473abd41effb784b03a
Gustavo <gustavopolonio1@gmail.com>
2024-12-27 22:09:08 -0300

    feat: add delete method on pets reporistory pattern

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -4,4 +4,5 @@ export interface PetsRepository {
   findById(id: string): Promise<Pet | null>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   save(pet: Pet): Promise<Pet>
+  delete(id: string): Promise<void>
 }
