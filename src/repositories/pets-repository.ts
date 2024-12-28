34106221776a614660b943967021bb5516a4eef1
Gustavo <gustavopolonio1@gmail.com>
2024-12-28 11:34:49 -0300

    feat: add filter by age in months, size and energy level in findManyByCity method on pets repository pattern

--- src/repositories/pets-repository.ts
+++ src/repositories/pets-repository.ts
@@ -1,8 +1,12 @@
-import { Pet, Prisma } from '@prisma/client'
+import { EnergyLevel, Pet, Prisma, Size } from '@prisma/client'
 
 export interface PetsRepository {
   findById(id: string): Promise<Pet | null>
-  findManyByCity(city: string, page: number): Promise<Pet[]>
+  findManyByCity(
+    city: string,
+    page: number,
+    filters?: { ageInMonths?: number; size?: Size; energyLevel?: EnergyLevel },
+  ): Promise<Pet[]>
   create(data: Prisma.PetUncheckedCreateInput): Promise<Pet>
   save(pet: Pet): Promise<Pet>
   delete(id: string): Promise<void>
