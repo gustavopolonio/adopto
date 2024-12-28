bfb52f00388b112c65cefd036574388c5b74e0d9
Gustavo <gustavopolonio1@gmail.com>
2024-12-27 22:10:55 -0300

    test(delete pet): add test for pet deletion

---
+++ src/use-cases/delete-pet.spec.ts
@@ -0,0 +1,45 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
+import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
+import { DeletePetUseCase } from './delete-pet'
+
+let orgsRepository: InMemoryOrgsRepository
+let petsRepository: InMemoryPetsRepository
+let sut: DeletePetUseCase
+
+describe('Delete pet use case', () => {
+  beforeEach(() => {
+    orgsRepository = new InMemoryOrgsRepository()
+    petsRepository = new InMemoryPetsRepository()
+    sut = new DeletePetUseCase(petsRepository)
+  })
+
+  it('should be able to delete a pet', async () => {
+    const org = await orgsRepository.create({
+      name: 'Org 1',
+      email: 'org1@test.test',
+      password_hash: await hash('123456', 6),
+      zip_code: '13566-583',
+      address: 'Rua Tomaz Antonio Gonzaga, 382',
+      city: 'São Carlos',
+      whatsapp: '16 99399-0990',
+    })
+
+    const pet = await petsRepository.create({
+      org_id: org.id,
+      name: 'Pet 1',
+      description: 'Description 1',
+      age_in_months: 12,
+      size: 'MEDIUM',
+      energy_level: 'HIGH',
+      adoption_requirements: ['Requirement 1'],
+    })
+
+    await sut.execute({
+      id: pet.id,
+    })
+
+    expect(petsRepository.pets[0].updated_at).not.toBeNull()
+  })
+})
