ea627d09c50712f43e6c950ea2c53b39627fa3e8
Gustavo <gustavopolonio1@gmail.com>
2024-12-28 22:46:44 -0300

    feat: create prisma orgs repository

---
+++ src/repositories/prisma/prisma-orgs-repository.ts
@@ -0,0 +1,57 @@
+import { Org, Prisma } from '@prisma/client'
+import { OrgsRepository } from '../orgs-repository'
+import { prisma } from '@/lib/prisma'
+
+export class PrismaOrgsRepository implements OrgsRepository {
+  async findById(id: string) {
+    const org = await prisma.org.findUnique({
+      where: {
+        id,
+        deleted_at: null,
+      },
+    })
+
+    return org
+  }
+
+  async findByEmail(email: string) {
+    const org = await prisma.org.findUnique({
+      where: {
+        email,
+        deleted_at: null,
+      },
+    })
+
+    return org
+  }
+
+  async create(data: Prisma.OrgCreateInput) {
+    const org = await prisma.org.create({
+      data,
+    })
+
+    return org
+  }
+
+  async save(data: Org) {
+    const org = await prisma.org.update({
+      where: {
+        id: data.id,
+      },
+      data,
+    })
+
+    return org
+  }
+
+  async delete(id: string) {
+    await prisma.org.update({
+      where: {
+        id,
+      },
+      data: {
+        deleted_at: new Date(),
+      },
+    })
+  }
+}
