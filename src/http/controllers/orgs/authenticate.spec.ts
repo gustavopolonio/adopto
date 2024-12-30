04be2918e4dde746681823f60852e30c4b185cf2
Gustavo <gustavopolonio1@gmail.com>
2024-12-30 09:33:42 -0300

    test(e2e): add test for org authentication

---
+++ src/http/controllers/orgs/authenticate.spec.ts
@@ -0,0 +1,26 @@
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { registerOrg } from '@/tests/utils/register-org'
+
+describe('Authenticate org (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate as an org', async () => {
+    const org = await registerOrg()
+
+    const response = await request(app.server).post('/sessions').send({
+      email: org.email,
+      password: org.password,
+    })
+
+    expect(response.status).toEqual(200)
+    expect(response.body.token).toEqual(expect.any(String))
+  })
+})
