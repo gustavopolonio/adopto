7b5613ef7b8d94f475bc6692a2be5b198141d518
Gustavo <gustavopolonio1@gmail.com>
2025-01-03 16:16:37 -0300

    feat: handle too large file error

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,22 @@
 import fastify from 'fastify'
 import fastifyJwt from '@fastify/jwt'
+import fastifyMultipart from '@fastify/multipart'
 import { ZodError } from 'zod'
 import { env } from './env'
 import { orgsRoutes } from './http/controllers/orgs/routes'
+import { MAX_FILE_SIZE } from './utils/constants'
 
 export const app = fastify()
 
 app.register(fastifyJwt, {
   secret: env.JWT_SECRET,
+})
+
+app.register(fastifyMultipart, {
+  attachFieldsToBody: false,
+  limits: {
+    fileSize: MAX_FILE_SIZE,
+  },
 })
 
 app.register(orgsRoutes)
@@ -17,6 +26,13 @@ app.setErrorHandler((error, _, reply) => {
     return reply.status(400).send({
       message: 'Validation error',
       issues: error.format(),
+    })
+  }
+
+  // Handling files too large
+  if (error.code === 'FST_REQ_FILE_TOO_LARGE') {
+    return reply.status(413).send({
+      message: 'File size limit reached',
     })
   }
 
---
+++ src/utils/constants.ts
@@ -0,0 +1,1 @@
+export const MAX_FILE_SIZE = 7 * 1024 * 1024 // 7 MB in bytes
