f17b3b9885d0f8183558f84a1a5c76f4e73447de
Gustavo <gustavopolonio1@gmail.com>
2025-01-06 16:16:19 -0300

    feat: create remove mthod on S3FileStorageProvider

--- src/providers/file-storage/implementations/s3-file-storage-provider.ts
+++ src/providers/file-storage/implementations/s3-file-storage-provider.ts
@@ -1,4 +1,7 @@
 import { Readable } from 'node:stream'
+import { DeleteObjectCommand } from '@aws-sdk/client-s3'
+import { s3Client } from '@/lib/aws-s3'
+import { env } from '@/env'
 import { FileStorageProvider } from '../file-storage-provider'
 import { uploadFileToS3 } from '@/utils/upload-file-to-s3'
 
@@ -18,5 +21,13 @@ export class S3FileStorageProvider implements FileStorageProvider {
     }
 
     return { fileUrl }
+  }
+
+  async remove(key: string) {
+    const deleteCommand = new DeleteObjectCommand({
+      Bucket: env.BUCKET_NAME,
+      Key: key,
+    })
+    await s3Client.send(deleteCommand)
   }
 }
