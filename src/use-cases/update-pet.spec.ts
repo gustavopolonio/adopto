7f4480c1f34118b186849b56219f59aa595502de
Gustavo <gustavopolonio1@gmail.com>
2025-01-08 14:59:03 -0300

    test(unit): fix test for update a pet without files change

--- src/use-cases/update-pet.spec.ts
+++ src/use-cases/update-pet.spec.ts
@@ -1,71 +1,36 @@
-import * as fs from 'fs'
-import { afterEach, beforeEach, describe, expect, it, vi } from 'vitest'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { hash } from 'bcryptjs'
+import { UpdatePetUseCase } from './update-pet'
+import { MockFileSorageProvider } from '@/providers/file-storage/implementations/mock-file-storage-provider'
 import { InMemoryOrgsRepository } from '@/repositories/in-memory/in-memory-orgs-repository'
 import { InMemoryPetsRepository } from '@/repositories/in-memory/in-memory-pets-repository'
-import { UpdatePetUseCase } from './update-pet'
-import { ResourceNotFoundError } from './errors/resource-not-found-error'
 import { InMemoryPhotosRepository } from '@/repositories/in-memory/in-memory-photos-repository'
-import { MultipartFile } from '@fastify/multipart'
-import { randomUUID } from 'node:crypto'
-import { Upload } from '@aws-sdk/lib-storage'
-import { generateFileHash } from '../utils/generate-file-hash'
 
-vi.mock('@aws-sdk/lib-storage', () => ({
-  Upload: vi.fn().mockImplementation(() => ({
-    done: vi.fn().mockResolvedValue({ Location: 'mocked-s3-url' }),
-  })),
-}))
-
-vi.mock('fs', () => ({
-  createReadStream: vi
-    .fn()
-    .mockImplementation((filePath) => `stream-of-${filePath}`),
-  readFileSync: vi
-    .fn()
-    .mockImplementation((filePath) =>
-      Buffer.from(`mock-content-of-${filePath}`),
-    ),
-}))
-
-const mockedPhotos = [
-  {
-    file: {
-      filename: 'photo1.jpg',
-      mimetype: 'image/jpeg',
-      encoding: '7bit',
-    } as MultipartFile,
-  },
-  {
-    file: {
-      filename: 'photo2.jpg',
-      mimetype: 'image/jpeg',
-      encoding: '7bit',
-    } as MultipartFile,
-  },
-]
-
-let orgsRepository: InMemoryOrgsRepository
 let petsRepository: InMemoryPetsRepository
+let orgsRepository: InMemoryOrgsRepository
 let photosRepository: InMemoryPhotosRepository
+let fileStorageProvider: MockFileSorageProvider
 let sut: UpdatePetUseCase
 
 describe('Update pet use case', () => {
   beforeEach(() => {
-    orgsRepository = new InMemoryOrgsRepository()
     petsRepository = new InMemoryPetsRepository()
+    orgsRepository = new InMemoryOrgsRepository()
     photosRepository = new InMemoryPhotosRepository()
-    sut = new UpdatePetUseCase(petsRepository, orgsRepository, photosRepository)
+    fileStorageProvider = new MockFileSorageProvider()
+    sut = new UpdatePetUseCase(
+      petsRepository,
+      orgsRepository,
+      photosRepository,
+      fileStorageProvider,
+    )
   })
 
-  afterEach(() => {
-    vi.clearAllMocks()
-  })
-
-  it('should be able to update a pet', async () => {
+  it('should be able to update a pet without files change', async () => {
     const org = await orgsRepository.create({
       name: 'Org 1',
       email: 'org1@test.test',
-      password_hash: '123456',
+      password_hash: await hash('123456', 6),
       zip_code: '13566-583',
       address: 'Rua Tomaz Antonio Gonzaga, 382',
       city: 'São Carlos',
@@ -73,176 +38,51 @@ describe('Update pet use case', () => {
     })
 
     const petCreated = await petsRepository.create({
+      org_id: org.id,
       name: 'Pet 1',
-      description: 'Pet 1 description',
-      age_in_months: 10,
+      description: 'Description 1',
+      age_in_months: 12,
       size: 'MEDIUM',
       energy_level: 'HIGH',
       adoption_requirements: ['Requirement 1'],
-      org_id: org.id,
-    })
-
-    await photosRepository.create({
-      pet_id: petCreated.id,
-      hash: randomUUID(),
-      s3_url: 's3-url.com',
     })
 
     const { pet } = await sut.execute({
       id: petCreated.id,
-      name: 'Pet 1 updated',
-      description: 'Pet 1 description updated',
-      ageInMonths: 12,
-      size: 'SMALL',
+      name: 'New pet 1',
+      description: 'Description 1',
+      ageInMonths: 10,
+      size: 'MEDIUM',
       energyLevel: 'LOW',
-      photos: mockedPhotos,
       adoptionRequirements: ['Requirement 2'],
-      orgId: petCreated.org_id,
+      orgId: org.id,
+      photos: [],
     })
 
-    expect(pet).toEqual(
+    expect(pet.id).toEqual(petCreated.id)
+    expect(petsRepository.pets[0]).toEqual(
       expect.objectContaining({
         id: petCreated.id,
+        name: 'New pet 1',
+        description: 'Description 1',
+        age_in_months: 10,
+        size: 'MEDIUM',
+        energy_level: 'LOW',
+        adoption_requirements: ['Requirement 2'],
         org_id: org.id,
-        name: 'Pet 1 updated',
+        photos: [],
       }),
     )
-    expect(Upload).toHaveBeenCalledTimes(2)
-    expect(fs.createReadStream).toHaveBeenCalledTimes(2)
-    expect(fs.createReadStream).toHaveBeenCalledWith('photo1.jpg')
-    expect(fs.createReadStream).toHaveBeenCalledWith('photo2.jpg')
-    expect(photosRepository.photos).toEqual([
-      expect.objectContaining({
-        s3_url: 's3-url.com',
-      }),
-      expect.objectContaining({
-        s3_url: 'mocked-s3-url',
-      }),
-      expect.objectContaining({
-        s3_url: 'mocked-s3-url',
-      }),
-    ])
+    expect(photosRepository.photos).toHaveLength(0)
   })
 
-  it('should not be able to update an unexisting pet', async () => {
-    const org = await orgsRepository.create({
-      name: 'Org 1',
-      email: 'org1@test.test',
-      password_hash: '123456',
-      zip_code: '13566-583',
-      address: 'Rua Tomaz Antonio Gonzaga, 382',
-      city: 'São Carlos',
-      whatsapp: '16 99399-0990',
-    })
+  // it('should be able to update a pet with files change', async () => {})
 
-    await expect(
-      sut.execute({
-        id: 'non-existing-pet-id',
-        name: 'Pet 1 updated',
-        description: 'Pet 1 description updated',
-        ageInMonths: 12,
-        size: 'SMALL',
-        energyLevel: 'LOW',
-        photos: mockedPhotos,
-        adoptionRequirements: ['Requirement 2'],
-        orgId: org.id,
-      }),
-    ).rejects.toBeInstanceOf(ResourceNotFoundError)
+  // it('should not be able to update an unexisting pet', async () => {})
 
-    expect(Upload).toHaveBeenCalledTimes(0)
-  })
+  // it('should not be able to update a pet of an unexisting org', async () => {})
 
-  it('should not be able to update a pet of an unexisting org', async () => {
-    const pet = await petsRepository.create({
-      name: 'Pet 1',
-      description: 'Pet 1 description',
-      age_in_months: 10,
-      size: 'MEDIUM',
-      energy_level: 'HIGH',
-      photos: ['photo1.jpg'],
-      adoption_requirements: ['Requirement 1'],
-      org_id: 'non-existing-org-id',
-    })
+  // it('should not be able to update a pet if file upload fails', async () => {})
 
-    await expect(
-      sut.execute({
-        id: pet.id,
-        name: 'Pet 1 updated',
-        description: 'Pet 1 description updated',
-        ageInMonths: 12,
-        size: 'SMALL',
-        energyLevel: 'LOW',
-        photos: mockedPhotos,
-        adoptionRequirements: ['Requirement 2'],
-        orgId: pet.org_id,
-      }),
-    ).rejects.toBeInstanceOf(ResourceNotFoundError)
-
-    expect(Upload).toHaveBeenCalledTimes(0)
-  })
-
-  it('should be able to update a pet without change repeated photos', async () => {
-    const org = await orgsRepository.create({
-      name: 'Org 1',
-      email: 'org1@test.test',
-      password_hash: '123456',
-      zip_code: '13566-583',
-      address: 'Rua Tomaz Antonio Gonzaga, 382',
-      city: 'São Carlos',
-      whatsapp: '16 99399-0990',
-    })
-
-    const petCreated = await petsRepository.create({
-      name: 'Pet 1',
-      description: 'Pet 1 description',
-      age_in_months: 10,
-      size: 'MEDIUM',
-      energy_level: 'HIGH',
-      adoption_requirements: ['Requirement 1'],
-      org_id: org.id,
-    })
-
-    const photoFilename = 'photo1.jpg'
-
-    await photosRepository.create({
-      pet_id: petCreated.id,
-      hash: generateFileHash(photoFilename),
-      s3_url: 's3-url.com',
-    })
-
-    const { pet } = await sut.execute({
-      id: petCreated.id,
-      name: 'Pet 1 updated',
-      description: 'Pet 1 description updated',
-      ageInMonths: 12,
-      size: 'SMALL',
-      energyLevel: 'LOW',
-      photos: [
-        {
-          file: {
-            filename: photoFilename,
-            mimetype: 'image/jpeg',
-            encoding: '7bit',
-          } as MultipartFile,
-        },
-      ],
-      adoptionRequirements: ['Requirement 2'],
-      orgId: petCreated.org_id,
-    })
-
-    expect(pet).toEqual(
-      expect.objectContaining({
-        id: petCreated.id,
-        org_id: org.id,
-        name: 'Pet 1 updated',
-      }),
-    )
-    expect(Upload).toHaveBeenCalledTimes(0)
-    expect(fs.createReadStream).toHaveBeenCalledTimes(0)
-    expect(photosRepository.photos).toEqual([
-      expect.objectContaining({
-        s3_url: 's3-url.com',
-      }),
-    ])
-  })
+  // it('should not be able to update a pet if file deletion fails', async () => {})
 })
