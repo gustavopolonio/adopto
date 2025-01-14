348ee709269734c4ab00308a4fa62a89ce611c72
Gustavo <gustavopolonio1@gmail.com>
2025-01-14 15:20:46 -0300

    style: format get pet profile controller

--- src/http/controllers/pets/profile.ts
+++ src/http/controllers/pets/profile.ts
@@ -1,7 +1,7 @@
-import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
-import { makeGetPetProfileUseCase } from '@/use-cases/factories/make-get-pet-profile-use-case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
+import { ResourceNotFoundError } from '@/use-cases/errors/resource-not-found-error'
+import { makeGetPetProfileUseCase } from '@/use-cases/factories/make-get-pet-profile-use-case'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
   const getPetProfileParamsSchema = z.object({
