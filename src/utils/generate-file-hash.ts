ded7db4b4aa243fffccca60a165ea24a30dba4ae
Gustavo <gustavopolonio1@gmail.com>
2025-01-14 12:05:44 -0300

    refactor: change param of generateFileHash function from Readable to Buffer

--- src/utils/generate-file-hash.ts
+++ src/utils/generate-file-hash.ts
@@ -1,14 +1,7 @@
 import { createHash } from 'node:crypto'
-import { Readable } from 'node:stream'
 
-export async function generateFileHash(file: Readable): Promise<string> {
-  return new Promise((resolve, reject) => {
-    const hash = createHash('sha256')
-
-    file.on('error', reject)
-    file.pipe(hash)
-
-    hash.on('finish', () => resolve(hash.digest('hex')))
-    hash.on('error', reject)
-  })
+export function generateFileHash(buffer: Buffer): string {
+  const hash = createHash('sha256')
+  hash.update(buffer)
+  return hash.digest('hex')
 }
